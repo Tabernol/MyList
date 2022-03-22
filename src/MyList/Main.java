1313c5f90a82f518a57288f951ddfc9ba1d6d9ee
Maks_race <madventurerace@gmail.com>
2022-03-22 12:08:25 +0200

    start2

--- src/MyList/Main.java
+++ src/MyList/Main.java
@@ -13,11 +13,15 @@ public class Main {
         a.addFirst(8);
         a.addFirst(9);
         a.add(2);
-        a.add(2,46);
-        a.add(0,1);
-        a.add(16,555);
-
-
+        a.add(2, 46);
+        a.add(0, 1);
+        a.add(15, 555);
+        a.remove(4);
+        a.remove(3);
+//        a.clear();
+        System.out.println(a.contains(4));
+        System.out.println(a.size());
+        System.out.println(a.isEmpty());
 
 
         a.showArray();
--- src/MyList/Task1/AList.java
+++ src/MyList/Task1/AList.java
@@ -14,7 +14,6 @@ public class AList {
     }
 
     public boolean add(Object item) {
-        array[count++] = item;
         if (count == array.length) {
             Object[] changeArray = new Object[capacity * 3 / 2 + 1];
             for (int i = 0; i < array.length; i++) {
@@ -23,11 +22,11 @@ public class AList {
             array = changeArray;
             capacity = array.length;
         }
+        array[count++] = item;
         return true;//тут все норм?
     }
 
     public boolean addFirst(Object item) {
-        count++;
         if (count == array.length) {
             Object[] changeArray = new Object[capacity * 3 / 2 + 1];
             for (int i = 0; i < array.length; i++) {
@@ -39,6 +38,7 @@ public class AList {
         for (int i = array.length - 2; i >= 0; i--) {
             array[i + 1] = array[i];
         }
+        count++;
         array[0] = item;
         return true;
     }
@@ -52,8 +52,8 @@ public class AList {
             array = changeArray;
             capacity = array.length;
         }
-        if(index<0 || index>=capacity){// тут без = викидається перша не моє виключення а Java
-            throw  new IndexOutOfBoundsException();
+        if (index < 0 || index >= capacity) {// тут без = викидається перша не моє виключення а Java
+            throw new IndexOutOfBoundsException();
         }
         for (int i = array.length - 2; i >= index; i--) {
             array[i + 1] = array[i];
@@ -61,6 +61,67 @@ public class AList {
         count++;
         array[index] = item;
         return true;
+    }
+
+    public boolean remove(Object item) {
+        boolean findObject = false;
+        for (int i = 0; i < array.length; i++) {// не працювало з циклом FOREACH&?????????? коли брав обєкт а не ячейку массиву
+            if (array[i] != null) {
+                if (array[i].equals(item)) {
+                    array[i] = null;
+                    findObject = true;
+                }
+            }
+        }
+        return findObject;
+    }
+
+    public Object remove(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= capacity) {
+            System.out.println("my exeption");
+            throw new IndexOutOfBoundsException();
+        }
+        Object object = array[index];
+        array[index] = null;
+        return object;
+    }
+
+    public void clear() {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = null;
+        }
+    }
+
+    public boolean contains(Object item) {
+        boolean availability = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                if (array[i].equals(item)) {
+                    availability = true;
+                }
+            }
+        }
+        return availability;
+    }
+
+    public int size() {
+        int count2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                count2++;
+            }
+        }
+        return count2;
+    }
+
+    public boolean isEmpty() {
+        boolean empty = true;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != null) {
+                empty = false;
+            }
+        }
+        return empty;
     }
 
     public void showArray() {
